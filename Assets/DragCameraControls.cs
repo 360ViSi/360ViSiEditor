e693915839f42e4fd14ca4ee5824579e9ee95e00
Niteb <ekq.sakke@gmail.com>
2020-11-13 19:28:12 +0200

    - Way to pan around the 360 video

---
+++ Assets/DragCameraControls.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragCameraControls : MonoBehaviour
+{
+    Transform cameraTransform = default;
+    Vector3 _oldMousePosition = default;
+    [SerializeField] float _scrollSpeed = 1f;
+    [SerializeField] float _maxAngle = 75;
+    private void Start() => cameraTransform = Camera.main.transform;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(2))
+            BeginCameraDrag();
+
+        if (Input.GetMouseButton(2))
+            DragCamera();
+    }
+
+    void BeginCameraDrag()
+    {
+        _oldMousePosition = Input.mousePosition;
+    }
+
+    void DragCamera()
+    {
+        var newMousePosition = Input.mousePosition;
+        var mouseDelta = Input.mousePosition - _oldMousePosition;
+
+        //Rotate cam, clamp in X-axis and always set Z-axis to zero, as some diagonal dragging can change it.
+        cameraTransform.Rotate(new Vector3(mouseDelta.y, -mouseDelta.x) * _scrollSpeed);
+        var camXRot = cameraTransform.localEulerAngles.x;
+        if (camXRot > _maxAngle && camXRot < 250)
+            cameraTransform.localEulerAngles = new Vector3(_maxAngle, cameraTransform.localEulerAngles.y, 0);
+        else if (camXRot < 360 - _maxAngle && camXRot > 250)
+            cameraTransform.localEulerAngles = new Vector3(-_maxAngle, cameraTransform.localEulerAngles.y, 0);
+        else
+            cameraTransform.localEulerAngles = new Vector3(cameraTransform.localEulerAngles.x, cameraTransform.localEulerAngles.y, 0);
+
+        _oldMousePosition = newMousePosition;
+    }
+}
