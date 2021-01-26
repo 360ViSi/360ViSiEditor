315269762fc9b30bfe41f0f14de70d988f40ec3c
Sakari Ekqvist <sakari.ekqvist@gmail.com>
2021-01-26 14:21:39 +0200

    - autoend actionnodes created and removed correctly - CanvasCameraControl drag working

--- Assets/Scripts/SimulationEditor/VideoNode.cs
+++ Assets/Scripts/SimulationEditor/VideoNode.cs
@@ -7,157 +7,165 @@ using TMPro;
 [System.Serializable]
 public class VideoNode : MonoBehaviour
 {
-  [SerializeField]
-  private int defaultVideoID = -2;
-  [SerializeField]private List<GameObject> actionGameObjects = new List<GameObject>();
+    [SerializeField]
+    private int defaultVideoID = -2;
+    [SerializeField] private List<GameObject> actionGameObjects = new List<GameObject>();
 
-  // video structure parameters
-  private int videoID=-2; //no ID -2 should give error when parsing
-  private string videoFileName = "None"; //no video file
-  private NodePort nodePort;
-  private StructureManager structureManager;
-  [SerializeField] private TMP_Text videoFilenameText;
+    // video structure parameters
+    private int videoID = -2; //no ID -2 should give error when parsing
+    private string videoFileName = "None"; //no video file
+    private NodePort nodePort;
+    private StructureManager structureManager;
+    [SerializeField] private TMP_Text videoFilenameText;
 
-  private bool loopingVideo;
-  private GameObject autoEndAction = null;
+    private bool loopingVideo;
+    private GameObject autoEndAction = null;
 
-  void Awake()
-  {
-    //set default videoID
-    videoID = defaultVideoID;
+    void Awake()
+    {
+        //set default videoID
+        videoID = defaultVideoID;
 
-    // get NodePort
-    nodePort = GetComponentInChildren<NodePort>();
-    structureManager = GetComponentInParent<StructureManager>();
-    if (nodePort==null) Debug.Log("There are no NodePort in "+ name);
-    if (structureManager == null) Debug.LogError("There is no StructureManager in parent");
-  }
-
-  public GameObject createNewActionNode(string actionText)
-  {
-    //get prefab from structureManager and initilize that
-    //add action node to the list and position it in UI
-    StructureManager structureManager = GetComponentInParent<StructureManager>();
-    GameObject newActionGameObject = Instantiate(structureManager.getActionNodePrefab(), GetComponent<RectTransform>());
-    actionGameObjects.Add(newActionGameObject);
-    RectTransform newRectTransform=newActionGameObject.GetComponent<RectTransform>();
-    newRectTransform.anchoredPosition = calculateActionNodePosition(newRectTransform);
-    newActionGameObject.GetComponent<ActionNode>().setActionText(actionText);
-    return newActionGameObject;
-  }
-
-  public void setVideoID(int newVideoID)
-  {
-    videoID = newVideoID;
-  }
-
-  public int getVideoID()
-  {
-    return videoID;
-  }
-
-  public void setVideoFileName(string newVideoFileName)
-  {
-    videoFileName = newVideoFileName;
-    videoFilenameText.text = videoFileName;
-  }
-  public string getVideoFileName()
-  {
-    return videoFileName;
-  }
-
-  public NodePort getNodePort()
-  {
-    return nodePort;
-  }
-
-  public bool getLoop()
-  {
-    return loopingVideo;
-  }
-
-  public void setLoop(bool value)
-  {
-    loopingVideo = value;
-    if(value){
-      autoEndAction = createNewActionNode("AUTOEND");
-      return;
+        // get NodePort
+        nodePort = GetComponentInChildren<NodePort>();
+        structureManager = GetComponentInParent<StructureManager>();
+        if (nodePort == null) Debug.Log("There are no NodePort in " + name);
+        if (structureManager == null) Debug.LogError("There is no StructureManager in parent");
     }
 
-    if(autoEndAction != null)
-      removeActionNode(autoEndAction);
-  }
-
-  public List<ActionNode> getActionNodeList()
-  {
-    List<ActionNode> actionNodes = new List<ActionNode>();
-    foreach (GameObject actionGameObject in actionGameObjects)
+    public void createNewActionNode()
     {
-      actionNodes.Add(actionGameObject.GetComponent<ActionNode>());
+        createNewActionNode("action", false);
     }
-    return actionNodes;
-  }
 
-  public void removeActionNode(GameObject actionGameObject)
-  {
-    if(actionGameObjects.Contains(actionGameObject))
-      actionGameObjects.Remove(actionGameObject);
-
-    Destroy(actionGameObject);
-
-    repositionActionNodes();
-  }
-  public void repositionActionNodes()
-  {
-    for (int i = 0; i < actionGameObjects.Count; i++)
+    private void createNewActionNode(string actionText, bool isAutoEnd)
     {
-      var actionNodeRectTransform = actionGameObjects[i].GetComponent<RectTransform>();
-      actionNodeRectTransform.anchoredPosition = calculateActionImagePosition(actionNodeRectTransform, i + 1);
-      actionGameObjects[i].GetComponent<ActionNode>().getNodePort().redraw();
+        //get prefab from structureManager and initilize that
+        //add action node to the list and position it in UI
+        StructureManager structureManager = GetComponentInParent<StructureManager>();
+        GameObject newActionGameObject = Instantiate(structureManager.getActionNodePrefab(), GetComponent<RectTransform>());
+        if(isAutoEnd) actionGameObjects.Insert(0, newActionGameObject);
+        else actionGameObjects.Add(newActionGameObject);
+        RectTransform newRectTransform = newActionGameObject.GetComponent<RectTransform>();
+        repositionActionNodes();
+        newActionGameObject.GetComponent<ActionNode>().setActionText(actionText);
+        if (isAutoEnd) autoEndAction = newActionGameObject;
     }
-    
-  }
 
-  private Vector2 calculateActionNodePosition(RectTransform newNodeRectTransform)
-  {
-    //Calculates position for the next actionNode GameObject
-    //Assumes that every action gameobject is in same size
-    //Uses rectTransforms scale and rect to calculate the actual size
-    Rect newNodeRect = newNodeRectTransform.rect;
-    Vector3 newNodeScale = newNodeRectTransform.localScale;
-    Vector2 realDimensions=new Vector2(newNodeRect.width*newNodeScale.x, newNodeRect.height*newNodeScale.y);
-    return new Vector2(realDimensions.x/2, -realDimensions.y*(actionGameObjects.Count-0.5f));
-  }
-
-  private Vector2 calculateActionImagePosition(RectTransform newNodeRectTransform, int position)
-  {
-    //Calculates position for the next action GameObject
-    //Assumes that every action gameobject is in same size
-    Rect newNodeRect = newNodeRectTransform.rect;
-    Vector3 newNodeScale = newNodeRectTransform.localScale;
-    Vector2 realDimensions=new Vector2(newNodeRect.width*newNodeScale.x, newNodeRect.height*newNodeScale.y);
-    return new Vector2(realDimensions.x/2, -realDimensions.y*(position-0.5f));
-  }
-
-  public void deleteNode(){
-    //delete connections to this video node & remove from simulation structure
-    nodePort.disconnect();
-    structureManager.removeVideoNode(gameObject);
-
-    //go through all the actions and remove them (disconnect first)
-    foreach (var item in actionGameObjects)
+    public void setVideoID(int newVideoID)
     {
-        item.GetComponent<ActionNode>().getNodePort().disconnect();
-        Destroy(item);
-        //S TODO: REMEMBER TO REFRESH ACTIONNODE INDEXING?
-    }    
+        videoID = newVideoID;
+    }
 
-    //delete this node only when it's not connected to anything
-    Destroy(gameObject);
-  }
+    public int getVideoID()
+    {
+        return videoID;
+    }
 
-  public void InspectorOpen()
-  {
-    NodeInspector.instance.CreateFields(this);
-  }
+    public void setVideoFileName(string newVideoFileName)
+    {
+        videoFileName = newVideoFileName;
+        videoFilenameText.text = videoFileName;
+    }
+    public string getVideoFileName()
+    {
+        return videoFileName;
+    }
+
+    public NodePort getNodePort()
+    {
+        return nodePort;
+    }
+
+    public bool getLoop()
+    {
+        return loopingVideo;
+    }
+
+    public void setLoop(bool value)
+    {
+        loopingVideo = value;
+        if (value)
+        {
+            createNewActionNode("AUTOEND", true);
+            return;
+        }
+
+        if (autoEndAction != null)
+            removeActionNode(autoEndAction);
+    }
+
+    public List<ActionNode> getActionNodeList()
+    {
+        List<ActionNode> actionNodes = new List<ActionNode>();
+        foreach (GameObject actionGameObject in actionGameObjects)
+        {
+            actionNodes.Add(actionGameObject.GetComponent<ActionNode>());
+        }
+        return actionNodes;
+    }
+
+    public void removeActionNode(GameObject actionGameObject)
+    {
+        if (actionGameObjects.Contains(actionGameObject))
+            actionGameObjects.Remove(actionGameObject);
+
+        Destroy(actionGameObject);
+
+        repositionActionNodes();
+    }
+    public void repositionActionNodes()
+    {
+        for (int i = 0; i < actionGameObjects.Count; i++)
+        {
+            var actionNodeRectTransform = actionGameObjects[i].GetComponent<RectTransform>();
+            actionNodeRectTransform.anchoredPosition = calculateActionImagePosition(actionNodeRectTransform, i + 1);
+            actionGameObjects[i].GetComponent<ActionNode>().getNodePort().redraw();
+        }
+
+    }
+
+    private Vector2 calculateActionNodePosition(RectTransform newNodeRectTransform)
+    {
+        //Calculates position for the next actionNode GameObject
+        //Assumes that every action gameobject is in same size
+        //Uses rectTransforms scale and rect to calculate the actual size
+        Rect newNodeRect = newNodeRectTransform.rect;
+        Vector3 newNodeScale = newNodeRectTransform.localScale;
+        Vector2 realDimensions = new Vector2(newNodeRect.width * newNodeScale.x, newNodeRect.height * newNodeScale.y);
+        return new Vector2(realDimensions.x / 2, -realDimensions.y * (actionGameObjects.Count - 0.5f));
+    }
+
+    private Vector2 calculateActionImagePosition(RectTransform newNodeRectTransform, int position)
+    {
+        //Calculates position for the next action GameObject
+        //Assumes that every action gameobject is in same size
+        Rect newNodeRect = newNodeRectTransform.rect;
+        Vector3 newNodeScale = newNodeRectTransform.localScale;
+        Vector2 realDimensions = new Vector2(newNodeRect.width * newNodeScale.x, newNodeRect.height * newNodeScale.y);
+        return new Vector2(realDimensions.x / 2, -realDimensions.y * (position - 0.5f));
+    }
+
+    public void deleteNode()
+    {
+        //delete connections to this video node & remove from simulation structure
+        nodePort.disconnect();
+        structureManager.removeVideoNode(gameObject);
+
+        //go through all the actions and remove them (disconnect first)
+        foreach (var item in actionGameObjects)
+        {
+            item.GetComponent<ActionNode>().getNodePort().disconnect();
+            Destroy(item);
+            //S TODO: REMEMBER TO REFRESH ACTIONNODE INDEXING?
+        }
+
+        //delete this node only when it's not connected to anything
+        Destroy(gameObject);
+    }
+
+    public void InspectorOpen()
+    {
+        NodeInspector.instance.CreateFields(this);
+    }
 }
